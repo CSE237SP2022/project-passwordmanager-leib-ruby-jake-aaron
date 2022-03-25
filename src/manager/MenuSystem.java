ccbc49c1a40958e84376ff57207bb228c46df295
jakekosowsky <jakekosowsky@gmail.com>
2022-03-25 12:06:38 -0500

    part 1

---
+++ src/manager/MenuSystem.java
@@ -0,0 +1,52 @@
+package manager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuSystem {
+	
+private List<String> menuOptions;
+private UserInput inputStream;
+
+	public MenuSystem() {
+		this.menuOptions = new ArrayList<String>();
+		this.menuOptions.add("1 Generate Password");
+		this.menuOptions.add("2 Exit");
+		this.inputStream = new UserInput();
+		
+	}
+	
+	public void showOptions() {
+		for(int i=0; i < this.menuOptions.size(); i++){
+            System.out.println(this.menuOptions.get(i));
+        }
+	}
+	
+	public boolean checkInput() {
+		
+		int value = this.inputStream.getInt("Select an Integer Option");
+		
+		while (value > 2 ) {
+			value = this.inputStream.getInt("Select a Valid Integer Option");
+		}
+		
+		if (value == 1) {
+			PasswordGenerator generator - new PasswordGenerator();
+			generator.generatePassword();
+			return true;
+		}
+		
+		if (value == 2) {
+			return false;
+		}
+		
+		return true;
+	}
+
+	public static void main(String[] args) {
+		MenuSystem menu = new MenuSystem();
+		menu.showOptions();
+		boolean output = menu.checkInput();
+	}
+
+}
