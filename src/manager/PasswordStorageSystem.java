aa55b404853262d2fc2fc906959f100e18e86847
AaronStein013 <ajstein013@gmail.com>
2022-04-06 13:54:30 -0500

    Created LoginData class to conveniently store all necessary data

--- src/manager/PasswordStorageSystem.java
+++ src/manager/PasswordStorageSystem.java
@@ -3,7 +3,16 @@ package manager;
 public class PasswordStorageSystem {
 
 	public PasswordStorageSystem() {
+		// make printwriter
+
+	}
+	
+	
+	
+	public boolean savePassword(LoginData entry) {
 		
+		
+		return true;
 	}
 
 }
