cb2ce793c98bb33576973141fa60b328eb6b8e0d
rubygrant <98493318+rubyalicegrant@users.noreply.github.com>
2022-04-11 13:26:15 -0500

    Storage System Ruby

--- src/manager/PasswordStorageSystem.java
+++ src/manager/PasswordStorageSystem.java
@@ -1,9 +1,24 @@
 package manager;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class PasswordStorageSystem {
-
-	public PasswordStorageSystem() {
-		
+	
+	private UserInput inputStream = new UserInput();
+	
+	public void printIDs() {
+		HashMap<String, LoginData> loginsMap = new HashMap<String, LoginData>();
+		Set<String> idSet = loginsMap.keySet();
+		for (String s : idSet) {
+			 System.out.println(s + "\n");
+		}
 	}
-
+	
+	public String getPassword() {
+		HashMap<String, LoginData> loginsMap = new HashMap<String, LoginData>();
+		String id = inputStream.getString("What website do you want the password for?");
+		LoginData currentLogin = loginsMap.get(id);
+		return currentLogin.password;
+	}
 }
