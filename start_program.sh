ba3ae2b01f26dfac39bb444e9ca7cf45ab3094c0
AaronStein013 <ajstein013@gmail.com>
2022-04-12 21:24:39 -0500

    added new classes to start script

--- start_program.sh
+++ start_program.sh
@@ -7,6 +7,8 @@ javac -d class -sourcepath src src/manager/UserInput.java
 javac -d class -sourcepath src src/manager/PasswordRequirements.java
 javac -d class -sourcepath src src/manager/PasswordGenerator.java
 javac -d class -sourcepath src src/manager/MenuSystem.java
+javac -d class -sourcepath src src/manager/LoginData.java
+javac -d class -sourcepath src src/manager/PasswordStorageSystem.java
 
 cd class/manager
 
