86c0aad603ada5937bc1c41cdf0925860f47311d
leibmalina <leib.malina@wustl.edu>
2022-04-21 15:10:32 -0500

    added edit and delete password methods

--- src/manager/MenuSystem.java
+++ src/manager/MenuSystem.java
@@ -15,10 +15,12 @@ private PasswordStorageSystem storageSystem;
 	public MenuSystem() {
 		this.menuOptions = new ArrayList<String>();
 		this.menuOptions.add("1 Generate Password");
-		this.menuOptions.add("2 View Saved Passwords");
+		this.menuOptions.add("2 View Saved Password");
 		this.menuOptions.add("3 Save New Password");
 		this.menuOptions.add("4 Evaluate Password");
-		this.menuOptions.add("5 Exit");
+		this.menuOptions.add("5 Change Password");
+		this.menuOptions.add("6 Delete Password");
+		this.menuOptions.add("7 Exit");
 		this.inputStream = new UserInput();
 		this.generator = new PasswordGenerator();
 		this.storageSystem = new PasswordStorageSystem();
@@ -35,7 +37,7 @@ private PasswordStorageSystem storageSystem;
 		
 		int value = this.inputStream.getInteger("Select an Integer Option");
 		
-		while (value > 5 || value < 1) {
+		while (value > 7 || value < 1) {
 			value = this.inputStream.getInteger("Select a Valid Integer Option");
 		}
 		
@@ -57,7 +59,12 @@ private PasswordStorageSystem storageSystem;
 			PasswordEvaluator evaluator = new PasswordEvaluator(storageSystem);
 			return evaluator.evaluatePassword();
 			}
-
+		else if (value == 5) {
+			return changePassword();
+		}
+		else if (value == 6) {
+			return deletePassword();
+		}
 		else {
 			return false;
 		}
@@ -77,14 +84,40 @@ private PasswordStorageSystem storageSystem;
 	}
 	
 	private boolean getPassword() {
-		System.out.println("You have passwords saved for the following websites:");
-		this.storageSystem.printIDs();
 		
-		String password = this.storageSystem.getPassword();
-		if(password.length() > 0) {
-			System.out.println("Password is: " + password + "\n");
+		String operationInsertString = "";
+		LoginData curLogin = this.storageSystem.accessLogin(operationInsertString);
+		if(curLogin != null) {
+			printPassword(curLogin);
 		}
-		
+		return true;
+	}
+	
+	private boolean changePassword() {
+		String operationInsertString = " to change";
+		LoginData oldLogin = this.storageSystem.accessLogin(operationInsertString);
+		System.out.println("Changing login for " + oldLogin.getKey() + ". Please enter the new details:");
+		LoginData newLogin = new LoginData();
+		newLogin.setAllEntryFields();
+		this.storageSystem.editLogin(oldLogin, newLogin);
+		System.out.println("Your login for " + newLogin.getKey() + " has been updated");
+		printPassword(newLogin);
+		return true;
+	}
+	
+	private void printPassword(LoginData curLogin) {
+		System.out.println("The login for " + curLogin.getKey() + " is:");
+		String username = curLogin.getUsername();
+		System.out.println("Username is: " + username);
+		String password = curLogin.getPassword();
+		System.out.println("Password is: " + password + "\n");
+	}
+	
+	private boolean deletePassword() {
+		String operationInsertString = " to delete";
+		LoginData curLogin = this.storageSystem.accessLogin(operationInsertString);
+		this.storageSystem.removeLogin(curLogin.getKey());
+		System.out.println("Password deleted for " + curLogin.getKey());
 		return true;
 	}
 	
